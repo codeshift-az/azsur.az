6700f589dda5f1ee6e07d447515e314e749b7fec
nihad memmedzade <nihad.mmmdzad@bk.ru>
2024-10-06 00:48:21 +0400

    About images added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,17 @@ export { default as LOGO } from "./logo/logo.png";
 
 // Backgrounds
 export { default as BreadcrumbBackground } from "./background/breadcrumb.jpg";
+
+// About sections
+
+// Banner 
+export {default as AboutBannerImg} from "./about/sections/banner/abo1.png";
+
+// About Us
+export {default as AboutUsImg} from "./about/sections/aboutUs/about1.jpg";
+
+// Our Teams
+export {default as OurTeamsImg} from "./about/sections/ourTeams/person1.jpg";
+export {default as OurTeamsImg2} from "./about/sections/ourTeams/person2.jpg";
+export {default as OurTeamsImg3} from "./about/sections/ourTeams/person3.jpg";
+export {default as OurTeamsImg4} from "./about/sections/ourTeams/person4.jpg";
